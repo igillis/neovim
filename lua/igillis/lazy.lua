1d4e03429dd4d92c33358666a403d9bbc85f1abd
Ian Gillis <iangillis@Ians-MBP-14-inch-4.local>
2024-03-28 08:04:43 +0700

    Follow current file

--- lua/igillis/lazy.lua
+++ lua/igillis/lazy.lua
@@ -83,6 +83,14 @@ require("lazy").setup({
 			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 			"MunifTanjim/nui.nvim",
 			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		opts = {
+				filesystem = {
+					follow_current_file = {
+						enabled = true,
+						leave_dirs_open = false,
+					}
+				}
 		}
 	}
 
